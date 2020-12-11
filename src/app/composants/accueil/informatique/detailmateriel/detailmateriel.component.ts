b3b886d381522919753e9aa8033492cab04ba7ab
iheb karoui <karouiiheb4@gmail.com>
2020-12-11 21:42:31 +0100

    Ajout d’un service

--- src/app/composants/accueil/informatique/detailmateriel/detailmateriel.component.ts
+++ src/app/composants/accueil/informatique/detailmateriel/detailmateriel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Materiel } from 'src/app/models/materiel';
+import { MaterielService } from 'src/app/services/materiel.service';
 
 @Component({
   selector: 'ihe-detailmateriel',
@@ -7,27 +8,17 @@ import { Materiel } from 'src/app/models/materiel';
   styleUrls: ['./detailmateriel.component.css']
 })
 export class DetailmaterielComponent implements OnInit {
-  det :Materiel ={
-    id: '2',
-    libelle: 'PC',
-    photo:'assets/Images/Informatique/pcFixe.jpg',
-    marque: 'HP',
-    prix: 1780.6,
-    description: 'PC Portable très pratique',
-    hautGamme: false,
-    quantite: 5,
-    commentaires:[
-    {contenu:' satisfaisant', note:3, auteur:'Sami MRAD', date:new Date(2020,11,4)},
-    {contenu:'Bon rapport qualité prix', note:4, auteur:'Samar Salah', date:new Date(2020,10,4)}]
-    };
+  det:Materiel;
     showing:boolean = true;
-  constructor() { }
-  onshow()
+    constructor(private materielServices : MaterielService) { }
+    onshow()
   {
     this.showing = !this.showing; 
   }
 
   ngOnInit(): void {
+    this.det = this.materielServices.getMaterielById("2") ; 
+
   }
 
 }
